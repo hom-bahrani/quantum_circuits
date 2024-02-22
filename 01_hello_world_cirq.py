6fd2dd8db4d8e38f046af197453121ecf867f404
hom-bahrani <hab0184@gmail.com>
2024-02-22 15:42:51 +0000

    update hello world example

--- 01_hello_world_cirq.py
+++ 01_hello_world_cirq.py
@@ -1,17 +1,34 @@
+# pylint: disable=wrong-or-nonexistent-copyright-notice
+"""Creates and simulates a simple circuit.
+
+=== EXAMPLE OUTPUT ===
+Circuit:
+(0, 0): ───X^0.5───M('m')───
+Results:
+m=11000111111011001000
+"""
+
 import cirq
 
-# Pick a qubit.
-qubit = cirq.GridQubit(0, 0)
 
-# Create a circuit that applies a square root of NOT gate, then measures the qubit.
-circuit = cirq.Circuit(cirq.X(qubit) ** 0.5, cirq.measure(qubit, key="m"))
-print("Circuit:")
-print(circuit)
+def main():
+    # Pick a qubit.
+    qubit = cirq.GridQubit(0, 0)
 
-# Simulate the circuit several times.
-simulator = cirq.Simulator()
-result = simulator.run(circuit, repetitions=20)
-print("Results:")
-print(result)
+    # Create a circuit
+    circuit = cirq.Circuit(
+        cirq.X(qubit) ** 0.5,
+        cirq.measure(qubit, key="m"),  # Square root of NOT.  # Measurement.
+    )
+    print("Circuit:")
+    print(circuit)
 
-# python -c 'import cirq_google; print(cirq_google.Sycamore)'
+    # Simulate the circuit several times.
+    simulator = cirq.Simulator()
+    result = simulator.run(circuit, repetitions=20)
+    print("Results:")
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
